3e2767c8c58f4735064150c41cb067b149d33a56
luhai <luhaiwork@gmail.com>
2014-07-10 13:58:01 +0800

    modify auto login script

--- autoLogin.py
+++ autoLogin.py
@@ -5,4 +5,5 @@ print ("开始输入用户名称")
 subprocess.call("adb shell input text test@qq.com")
 subprocess.call("adb shell input keyevent 66")
 print ("开始输入用户密码")
-subprocess.call("adb shell input text 111111");
+subprocess.call("adb shell input text 111111")
+subprocess.call("adb shell input keyevent 66")
